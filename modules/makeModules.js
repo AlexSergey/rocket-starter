c7bd6b0b31190e53e264fb2fcbbc43db32ace5d9
Sergey Aleksandrov <gooddev.sergey@gmail.com>
2019-08-20 20:19:50 +0300

    1. Fix error on lib compile

--- modules/makeModules.js
+++ modules/makeModules.js
@@ -11,7 +11,6 @@ function babelOpts(isNodejs = false) {
         presets: [
             [require.resolve('@babel/preset-env'), Object.assign({
                 useBuiltIns: 'usage',
-                corejs: 3,
                 modules: false,
                 loose: true,
             }, isNodejs ? {
